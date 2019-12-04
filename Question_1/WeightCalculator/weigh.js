bc457ade96cb02055b0c469a6f1506552888a6ca
NishkarshSingh02 <nishkarsh.s@testyantra.in>
2019-12-04 15:24:47 +0530

    JS Test

---
+++ Question_1/WeightCalculator/weigh.js
@@ -0,0 +1,35 @@
+
+
+
+    function kilo() {
+        document.getElementById('convert').innerHTML =
+            (document.getElementById('grams').value/1000 + "kg");
+    }
+
+    // function gram() {
+    //     document.getElementById('convert').innerHTML =
+    //         (document.getElementById('grams').value*1000 + "kg");
+    // }
+
+
+
+
+    ////////////////////////////////////////
+
+
+
+    
+// Enter weight here: <input type="text" name="grams" id="grams">
+// <br>
+// Results: <p id="convert"></p>
+
+// <button onclick="kilo()">Kilogram</button>
+
+// <script type="text/javascript">
+
+//     function kilo() {
+//         document.getElementById('convert').innerHTML =
+//             (document.getElementById('grams').value/1000 + "kg");
+//     }
+
+// </script>
